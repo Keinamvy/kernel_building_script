b62774b7fc1cdc7ac5a4cfa251680a133fd8c632
Keinamvy <92191018+Keinamvy@users.noreply.github.com>
2025-06-22 15:35:33 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -48,20 +48,20 @@ rm -rf error.log
 if [ "$DEVICE_TYPE" == sweet2  ];
 then
 DEVICE="REDMI NOTE 12 PRO 4G"
-KERNEL_NAME="POSITRON_KERNEL"
-CODENAME="SWEET-K6A"
+KERNEL_NAME="Nightmare"
+CODENAME="Sweet_k6a"
 
 DEFCONFIG_COMMON="vendor/sdmsteppe-perf_defconfig"
-DEFCONFIG_DEVICE="vendor/sweet2.config"
+DEFCONFIG_DEVICE="vendor/sweetk6a.config"
 
 AnyKernel="https://github.com/Keinamvy/AnyKernel3.git"
-AnyKernelbranch="positron"
+AnyKernelbranch="nightmare"
 fi
 
 # Kernel build release tag
 KRNL_REL_TAG="$KERNEL_TAG"
 
-HOSST="positron"
+HOSST="nightmare"
 USEER="Keinamvy"
 
 # setup telegram env
